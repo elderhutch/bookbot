fcbf06fad04f321b759f7c653923b150b3319c1e
elderhutch <elderhutch@outlook.com>
2025-01-13 09:20:41 -0500

    Character totals working

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     books_path = "books/frankenstein.txt"
     text = book(books_path)
     total_words = word_count(text)
+    character_totals = character_count(text)
     print(total_words)
+    print(character_totals)
 
 def book(books_path):
     with open(books_path) as f:
@@ -15,5 +17,16 @@ def word_count(text):
     for i in split:
         counter += 1
     return counter
+
+def character_count(text):
+    unique_dict = {}
+    for char in text:
+        lowercase = char.lower()
+        if lowercase not in unique_dict:
+            unique_dict[lowercase] = 1
+        else:
+            unique_dict[lowercase] += 1
+    return unique_dict
+
 
 main()
